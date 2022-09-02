b12a517bd101c063d98f2aa514da860691d400d2
Sérgio Roberto Delfino <srdelfino@gmail.com>
2022-09-02 17:48:58 -0300

    Teste

--- repositorio/run.bat
+++ repositorio/run.bat
@@ -1,3 +1,5 @@
+mvn install:install-file -DgroupId=com.oracle -DartifactId=ojdbc14 -Dversion=10.2.0.5 -Dfile="D:\Desenvolvimento\Bibliotecas\ojdbc14-10.2.0.5.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
 mvn install:install-file -DgroupId=org.apache.shiro -DartifactId=shiro-faces -Dversion=2.0 -Dfile="D:\Desenvolvimento\Bibliotecas\shiro-faces-2.0.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
 
 mvn install:install-file -DgroupId=org.primefaces.themes -DartifactId=rio -Dversion=3.1.3 -Dfile="G:\Meu Drive\Temp\Temas\rio-layout-3.1.3\rio-theme-3.1.3.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
