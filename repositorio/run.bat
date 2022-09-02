a730fa6228bcf019cf29d1035c24d8a2cb643182
Sérgio Roberto Delfino <srdelfino@gmail.com>
2022-09-02 16:39:48 -0300

    Teste

--- repositorio/run.bat
+++ repositorio/run.bat
@@ -1,1 +1,3 @@
+mvn install:install-file -DgroupId=org.apache.shiro -DartifactId=shiro-faces -Dversion=2.0 -Dfile="D:\Desenvolvimento\Bibliotecas\shiro-faces-2.0.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
 mvn install:install-file -DgroupId=org.primefaces.themes -DartifactId=rio -Dversion=3.1.3 -Dfile="G:\Meu Drive\Temp\Temas\rio-layout-3.1.3\rio-theme-3.1.3.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
