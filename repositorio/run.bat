4e13fc37ac86cd375a6c32b31fed0b58745b6971
Sérgio Roberto Delfino <srdelfino@gmail.com>
2022-09-05 22:03:49 -0300

    Update

--- repositorio/run.bat
+++ repositorio/run.bat
@@ -1,5 +1,7 @@
 mvn install:install-file -DgroupId=com.oracle -DartifactId=ojdbc14 -Dversion=10.2.0.5 -Dfile="D:\Desenvolvimento\Bibliotecas\ojdbc14-10.2.0.5.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
 
+mvn install:install-file -DgroupId=com.oracle -DartifactId=ojdbc7 -Dversion=12.1.0.1 -Dfile="D:\Desenvolvimento\Bibliotecas\ojdbc7-12.1.0.1.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
 mvn install:install-file -DgroupId=org.jrimum -DartifactId=jrimum-bopepo-local -Dversion=0.3-Litio-GeracaoGuia-VersaoAvaliacao-2011-04-14 -Dfile="D:\Desenvolvimento\Bibliotecas\Bopepo-0.3-Litio-GeracaoGuia-VersaoAvaliacao-2011-04-14.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
 
 mvn install:install-file -DgroupId=org.jrimum -DartifactId=jrimum-bopepo -Dversion=SNAPSHOT-20110415-bin -Dfile="D:\Desenvolvimento\Bibliotecas\JRimum-Bopepo-SNAPSHOT-20110415-bin.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
