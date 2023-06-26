a5e13c6c73f350b380e6303d77523a0fe2f4a90b
alexandreavg <alexandre-avg@hotmail.com>
2023-06-26 10:17:56 -0300

    activation

--- repositorio/run.bat
+++ repositorio/run.bat
@@ -21,3 +21,33 @@ mvn install:install-file -DgroupId=org.primefaces.themes -DartifactId=ultima -Dv
 mvn install:install-file -DgroupId=com.oracle -DartifactId=ojdbc7 -Dversion=11.2.0.4 -Dfile=C:\Dependencias\ojdbc6-11.2.0.4\ojdbc6-11.2.0.4.jar  -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
 
 mvn install:install-file -DgroupId=br.com.gemmap.zsdk -DartifactId=ZSDK_ANDROID_API -Dversion=1.0.0 -Dfile="C:\Users\agarcia\Documents\GitHub\Agua\LISA\app\libs\ZSDK_ANDROID_API.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+###
+
+mvn install:install-file -DgroupId=br.com.gemmap.lisa -DartifactId=activation -Dversion=1.0.0 -Dfile="C:\Users\agarcia\Documents\GitHub\Agua\LISA\app\libs\activation.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=br.com.gemmap.lisa -DartifactId=additionnal -Dversion=1.0.0 -Dfile="C:\Users\agarcia\Documents\GitHub\Agua\LISA\app\libs\additionnal.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=br.com.gemmap.lisa -DartifactId=commons-io-2.2 -Dversion=2.2 -Dfile="C:\Users\agarcia\Documents\GitHub\Agua\LISA\app\libs\commons-io-2.2.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=br.com.gemmap.lisa -DartifactId=commons-lang3-3.4 -Dversion=3.4 -Dfile="C:\Users\agarcia\Documents\GitHub\Agua\LISA\app\libs\commons-lang3-3.4.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=br.com.gemmap.lisa -DartifactId=commons-commons-net-3.1 -Dversion=3.1 -Dfile="C:\Users\agarcia\Documents\GitHub\Agua\LISA\app\libs\commons-net-3.1.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=br.com.gemmap.lisa -DartifactId=commons-validator-1.4.0 -Dversion=1.4.0 -Dfile="C:\Users\agarcia\Documents\GitHub\Agua\LISA\app\libs\commons-validator-1.4.0.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=br.com.gemmap.lisa -DartifactId=httpcore-4.3.1 -Dversion=4.3.1 -Dfile="C:\Users\agarcia\Documents\GitHub\Agua\LISA\app\libs\httpcore-4.3.1.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=br.com.gemmap.lisa -DartifactId=httpmime-4.3.2 -Dversion=4.3.2 -Dfile="C:\Users\agarcia\Documents\GitHub\Agua\LISA\app\libs\httpmime-4.3.2.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=br.com.gemmap.lisa -DartifactId=jackson-annotations-2.2.3 -Dversion=2.2.3 -Dfile="C:\Users\agarcia\Documents\GitHub\Agua\LISA\app\libs\jackson-annotations-2.2.3.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=br.com.gemmap.lisa -DartifactId=jackson-core-2.2.3 -Dversion=2.2.3 -Dfile="C:\Users\agarcia\Documents\GitHub\Agua\LISA\app\libs\jackson-jackson-core-2.2.3.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=br.com.gemmap.lisa -DartifactId=jackson-databind-2.2.3 -Dversion=2.2.3 -Dfile="C:\Users\agarcia\Documents\GitHub\Agua\LISA\app\libs\jackson-databind-2.2.3.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=br.com.gemmap.lisa -DartifactId=mail -Dversion=1.0.0 -Dfile="C:\Users\agarcia\Documents\GitHub\Agua\LISA\app\libs\mail.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=br.com.gemmap.lisa -DartifactId=opencsv-2.2 -Dversion=2.2 -Dfile="C:\Users\agarcia\Documents\GitHub\Agua\LISA\app\libs\opencsv-2.2.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=br.com.gemmap.lisa -DartifactId=snmp6_1 -Dversion=1.0.0 -Dfile="C:\Users\agarcia\Documents\GitHub\Agua\LISA\app\libs\snmp6_1.jar" -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
